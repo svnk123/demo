803d69d62f7f69c3949922c88a42e977f847f7bb
swethaqa654 <swethaqa654@gmail.com>
2021-09-23 16:49:30 -0500

    init commit

--- src/main/java/com/example/demo/controller/ExmpleController.java
+++ src/main/java/com/example/demo/controller/ExmpleController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
 public class ExmpleController {
 
 	@GetMapping("/all")
-	public void getSearchResult() {
+	public  ResponseEntity<String> getSearchResult() {
 		//String uri = "https://apiprod.eip.tractorsupply.com/getAccessToken?app_id=3f82abb4-734a-4656-8683-13a6ef1b5d2e";
 		String uri = "https://qa3-api.tractorsupply.com/getAccessToken?app_id=0658c9c4-4922-44fb-a07f-e39c9b2d134d";
 	    RestTemplate restTemplate = new RestTemplate();
@@ -20,5 +22,7 @@ public class ExmpleController {
 	    System.out.println(result);
 		
 		System.err.println("Changes made to the URI Demo...");
+		
+		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 }
