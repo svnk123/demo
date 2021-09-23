df5a46e4bb2f139b934661337cf8c663cf993a15
swethaqa654 <swethaqa654@gmail.com>
2021-09-23 16:08:49 -0500

    init commit

--- src/main/java/com/example/demo/controller/ExmpleController.java
+++ src/main/java/com/example/demo/controller/ExmpleController.java
@@ -12,8 +12,8 @@ public class ExmpleController {
 
 	@GetMapping("/all")
 	public void getSearchResult() {
-		String uri = "https://apiprod.eip.tractorsupply.com/getAccessToken?app_id=3f82abb4-734a-4656-8683-13a6ef1b5d2e";
-		//String uri = "https://qa3-api.tractorsupply.com/getAccessToken?app_id=0658c9c4-4922-44fb-a07f-e39c9b2d134d";
+		//String uri = "https://apiprod.eip.tractorsupply.com/getAccessToken?app_id=3f82abb4-734a-4656-8683-13a6ef1b5d2e";
+		String uri = "https://qa3-api.tractorsupply.com/getAccessToken?app_id=0658c9c4-4922-44fb-a07f-e39c9b2d134d";
 	    RestTemplate restTemplate = new RestTemplate();
 	    String result = restTemplate.getForObject(uri, String.class);
 
